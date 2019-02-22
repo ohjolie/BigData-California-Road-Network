735981a5acf0b8d859fcd03e2fc3b7000fd418ca
Jolie <zhuli0413@gmail.com>
2019-02-21 21:50:38 -0500

    update graph reduce

--- project1.R
+++ project1.R
@@ -1,3 +1,4 @@
+#attention: load sna first, then load igraph
 #read data,need to set working directory first
 edges1 <- read.table('roadNet-CA.txt') 
 em <- as.matrix(edges1) #save data in matrix
@@ -7,29 +8,22 @@ v2 <- em[,2] #Save the second column of data to v2
 relations <- data.frame(from=v1,to=v2) 
 g<-graph.data.frame(relations,directed=TRUE)
 
-node <- V(g) #save all the node in g to "node"
-View(g) # show g
-nodefreq <- as.data.frame(table(v1))
-#need to reduce the graph size here
-
-g <- g - V(g)[degree(g)<=8]
-plot(g)
+#reduce the graph size here
+#delete all the node with degree 1,2,3,4,5
+g <- g - V(g)[degree(g)<=10]
+# generate the plot
+plot(g) 
 
 
 
-# remove(node)
-# node <- V(g)
-#delete all the node with degree 1
-# #n <- 0
-# m <-attr(node,"names")
-# tempnode <- as.numeric(m)
-# for (i in tempnode) {
+#node <- V(g) #save all the node in g to "node"
+#nodefreq <- as.data.frame(table(v1))
+# charnode <-attr(node,"names")
+# intnode <- as.numeric(charnode)
+# for (i in intnode) {
 #   if(nodefreq[nodefreq$v1==i,2]==1L)
-#     {print(i)}
+#     {#delete_vertices(g,i)}
 # }
 
-#delete_vertices(g,i)
 #View(g)
-
-#plot(g) # generate the plot
   
